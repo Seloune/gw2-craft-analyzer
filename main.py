cb0a3356f26f6b2cbc344a6840c0da0041757940
David <misucat7777@gmail.com>
2026-07-28 23:45:19 +0200

    v0.1 - Initialisation du projet

---
+++ main.py
@@ -0,0 +1,11 @@
+# Craft Analizer Guild Wars 2
+
+# main.py
+# Version : 0.1
+
+def main():
+    nom_objet = input("Entrez l'ID d'un objet que vous souhaitez analyser : ")
+    print(f"Objet recherché : {nom_objet}")
+
+if __name__ == "__main__":
+    main()

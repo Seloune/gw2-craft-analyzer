470e7a868209ab0b7ad39cd94edb8d93d4e4fa7e
David <misucat7777@gmail.com>
2026-07-29 00:55:42 +0200

    v0.3 - Ajout de la récupération des prix du Comptoir

--- functions/api.py
+++ functions/api.py
@@ -1,14 +1,24 @@
 #========================================
 # Craft Analyzer Guild Wars 2
 # api.py
-# version 0.1
+# version 0.3
 #========================================
 
 import requests
 
 def recuperer_objet(id_objet):
     url = f"https://api.guildwars2.com/v2/items/{id_objet}?lang=fr"
-    reponse = requests.get(url)
+    reponse = requests.get(url, timeout=5)
+
+    if reponse.status_code == 200:
+        return reponse.json()
+
+    return None
+
+
+def recuperer_prix(id_objet):
+    url = f"https://api.guildwars2.com/v2/commerce/prices/{id_objet}"
+    reponse = requests.get(url, timeout=5)
 
     if reponse.status_code == 200:
         return reponse.json()
--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 #========================================
 # Craft Analyzer Guild Wars 2
 # main.py
-# version 0.2
+# version 0.3
 #========================================
 
-from functions.api import recuperer_objet
+from functions.api import recuperer_objet, recuperer_prix
+
+separateur = "--------------------------------------------------------------"
 
 
 def main():
+    print("")
     id_objet = input("Entrez l'ID d'un objet que vous souhaitez analyser : ")
     objet = recuperer_objet(id_objet)
 
     if objet is not None:
-        print(f"{objet['name']}")
+        print(separateur)
+        print(objet["name"])
+
+        objet_prix = recuperer_prix(id_objet)
+
+        if objet_prix is not None:
+            print(f"Prix d'achat : {objet_prix['buys']['unit_price']}")
+            print(f"Prix de vente : {objet_prix['sells']['unit_price']}")
+        else:
+            print("Prix indisponible.")
+
+        print(separateur)
+
     else:
-        print("objet introuvable ou erreur lors de la requête.")
+        print("Objet introuvable ou erreur lors de la requête.")
 
 
 if __name__ == "__main__":
     main()
+
+print("")
